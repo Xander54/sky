f5faeaca4cdbe8e50a4adbc099e9dd925404be92
Xander54 <97338250+Xander54@users.noreply.github.com>
2023-09-12 11:11:54 +0100

    Code commit

---
+++ polls/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.utils import timezone
+import datetime
+from .models import Question
+from django.template import loader
+
+# Create your views here.
+def index(request):
+    latest_question_list=Question.objects.order_by("-pub_date")
+    context={
+        'latest_question_list':latest_question_list,
+    }
+    
+    return render(request,'polls/index.html',context)
+
+def detail(request, question_id):
+    return HttpResponse(f"You're looking at question{question_id}.")
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
